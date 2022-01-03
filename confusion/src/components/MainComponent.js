78086ebdade9983ec68c8fd52a91f3e66315efce
Mohammed Amir Hossain <amiradhocenterprise@gmail.com>
2022-01-03 17:18:43 +0900

    Assignment 2

--- confusion/src/components/MainComponent.js
+++ confusion/src/components/MainComponent.js
@@ -1,59 +1,87 @@
-import React, { Component } from 'react';
-import Home from './HomeComponent';
-import Menu from './MenuComponent';
-import Contact from './ContactComponent';
-import DishDetail from './DishdetailComponent';
+import React from 'react';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
-import { DISHES } from '../shared/dishes';
-import { COMMENTS } from '../shared/comments';
-import { PROMOTIONS } from '../shared/promotions';
-import { LEADERS } from '../shared/leaders';
-import { Switch, Route, Redirect } from 'react-router-dom';
+import Home from './HomeComponent'
+import Menu from './MenuComponent';
+import Contact from './ContactComponent';
+import About from './AboutComponent';
+import DishDetail from './DishdetailComponent';
+import {DISHES} from '../shared/dishes';
+import {COMMENTS} from '../shared/comments';
+import {LEADERS} from '../shared/leaders';
+import {PROMOTIONS} from '../shared/promotions';
+import {Switch, Route, Redirect} from  'react-router-dom';
 
-class Main extends Component {
 
-  constructor(props) {
+class Main extends React.Component {
+  constructor(props){
     super(props);
-
-    this.state = {
+    this.state ={
       dishes: DISHES,
       comments: COMMENTS,
       promotions: PROMOTIONS,
       leaders: LEADERS
-    };
+    }
   }
 
-  render() {
+  onDishSelect(dishId){
+    this.setState({selectedDish: dishId});
+  }
 
-    const HomePage = () => {
+  render(){
+    const HomePage =()=>{
       return(
-          <Home 
-              dish={this.state.dishes.filter((dish) => dish.featured)[0]}
-              promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
-              leader={this.state.leaders.filter((leader) => leader.featured)[0]}
-          />
+        <Home
+          dish= {this.state.dishes.filter((dish)=>dish.featured)[0]}
+          promotion = {this.state.promotions.filter((promotion)=>promotion.featured)[0]}
+          leader={this.state.leaders.filter((leader)=>leader.featured)[0]}
+        />
       );
     }
 
-    const DishWithId = ({match}) => {
+    const ContactPage =()=>{
       return(
-          <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
-            comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+        <Contact/>
       );
-    };
+    }
+
+    const AboutPage =()=>{
+      return(
+        <About leaders={this.state.leaders}/>
+      );
+    }
+
+
+
+    const DishWithId =({match})=>{
+      return(
+        <DishDetail
+          dish={this.state.dishes.filter((dish)=> dish.id===parseInt(match.params.dishId,10))[0]}
+          comments={this.state.comments.filter((comment)=>comment.dishId===parseInt(match.params.dishId,10))}
+          >
+        </DishDetail>
+      );
+    }
+
 
     return (
       <div>
-        <Header />
+        <Header/>
         <Switch>
-              <Route path='/home' component={HomePage} />
-              <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
-              <Route path='/menu/:dishId' component={DishWithId} />              
-              <Route exact path='/contactus' component={Contact} />
-              <Redirect to="/home" />
-          </Switch>
-        <Footer />
+          <Route path="/home" component={HomePage}/>
+          <Route exact path="/menu"
+          component={()=>
+            <div>
+              <Menu dishes={this.state.dishes} onClick={(dishId)=>this.onDishSelect(dishId)}/>
+              <DishDetail className="text-center" dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]}></DishDetail>
+            </div>
+          }/>
+        <Route path="/menu/:dishId" component={DishWithId}/>
+        <Route exact path="/contactus" component={ContactPage}/>
+        <Route exact path="/aboutus" component={AboutPage}/>
+        <Redirect to="/home"/>
+        </Switch>
+        <Footer/>
       </div>
     );
   }
