a78bbbf865345830f1df73be97821f555c3256e9
Mohammed Amir Hossain <amiradhocenterprise@gmail.com>
2022-02-06 23:14:16 +0900

    Fetch from Server

--- confusion/src/components/DishdetailComponent.js
+++ confusion/src/components/DishdetailComponent.js
@@ -5,6 +5,7 @@ import { Card, CardImg, CardText, CardBody,
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Link } from 'react-router-dom';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 const required =(val) =>val&&val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -91,12 +92,12 @@ export class CommentForm extends Component {
 }
 
 
-    function RenderDish({dish}){
+    function RenderDish({dish}) {
         if (dish != null) {
             return(
                 <div class='col12 col-md-5 m-1'>
                     <Card>
-                        <CardImg width="100%" src={dish.image} alt={dish.name} />
+                    <CardImg top src={baseUrl + dish.image} alt={dish.name} />
                         <CardBody>
                             <CardTitle heading>{dish.name}</CardTitle>
                             <CardText>{dish.description}</CardText>
